9261edc230d5c99d025bf7c8cab2a658ff7e324a
김시원 / Kim Siwon <114036799+SIWON990327@users.noreply.github.com>
2025-01-28 13:32:12 +0900

    [Refactor] 웹소켓 URL ws로 변경 #22
    * [Refactor] 채팅 Response 수정 #20
    
    * [Refactor] 웹소켓 URL ws로 변경 #22

--- src/main/java/kdt/web_ide/common/config/WebSocketConfig.java
+++ src/main/java/kdt/web_ide/common/config/WebSocketConfig.java
@@ -24,7 +24,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chatting").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/chatting").setAllowedOriginPatterns("*");
     }
 
     @Override
