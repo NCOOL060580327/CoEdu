c7483452037592733cee0ab70a9d559d9fab8239
김시원 / Kim Siwon <114036799+SIWON990327@users.noreply.github.com>
2025-01-30 18:16:17 +0900

    [Refactor] : 채팅 Request 변경 #29
    * [Refactor] : 채팅 Request 변경 #29
    
    * [Refactor] ChatService 로그 제거

--- src/main/java/kdt/web_ide/chat/service/ChatService.java
+++ src/main/java/kdt/web_ide/chat/service/ChatService.java
@@ -13,7 +13,10 @@ import kdt.web_ide.common.exception.ErrorCode;
 import kdt.web_ide.members.entity.Member;
 import kdt.web_ide.members.entity.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -35,12 +38,14 @@ public class ChatService {
 
     // 메세지 전송
     @Transactional
-    public void sendMessage(Long chatRoomId, Long senderId, String messageText) {
+    public void sendMessage(Long chatRoomId, String messageText) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         ChatRoom chatRoom = chatRoomRepository.findById(chatRoomId)
                 .orElseThrow(() -> new CustomException(ErrorCode.CHATROOM_NOT_FOUND));
 
-        Member sender = memberRepository.findById(senderId)
+        Member sender = memberRepository.findByLoginId(authentication.getName())
                 .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
 
         ChatMessage message = saveMessage(chatRoom, sender, messageText);
@@ -49,9 +54,9 @@ public class ChatService {
             simpMessagingTemplate.convertAndSend("/room/" + chatRoomId, GetChatMessageResponseDto.fromChatMessage(message));
         });
 
-        chatRoomMemberRepository.incrementNotReadCount(chatRoomId, senderId);
+        chatRoomMemberRepository.incrementNotReadCount(chatRoomId, sender.getMemberId());
 
-        notifyUnreadMessageCount(chatRoomId, senderId);
+        notifyUnreadMessageCount(chatRoomId, sender.getMemberId());
     }
 
     @Transactional
